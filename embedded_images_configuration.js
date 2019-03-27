fef3993f1f9349876a5585b8043a87b4636666d5
hayleystevens <hayley.stevens@slalom.com>
2019-03-27 16:45:22 -0700

    updates again

--- embedded_images_configuration.js
+++ embedded_images_configuration.js
@@ -21,14 +21,15 @@
       $('#interval').val(openPayload);
       $('#closeButton').click(closeDialog);
 
-      let worksheets = tableau.extensions.dashboardContent.dashboard.worksheets ;
+      let dashboard = tableau.extensions.dashboardContent.dashboard ;
       let visibleWorksheets = [];
       selectedWorksheets = parseSettingsForActiveWorksheets();
 
       // Loop through datasources in this sheet and create a checkbox UI 
       // element for each one.  The existing settings are used to 
       // determine whether a datasource is checked by default or not.
-      worksheets.forEach(function (worksheet) {
+        worksheets.forEach(function (worksheet) {
+        worksheets.forEach(function (worksheet) {
             let isActive = (selectedWorksheets.indexOf(worksheet.id) >= 0);
 
             if (visibleWorksheets.indexOf(worksheet.id) < 0) {
@@ -38,6 +39,7 @@
           });
         });
       });
+    });
 
   /**
    * Helper that parses the settings from the settings namesapce and 
